a52caaa0a15605ca6339f565db3e3a772ad42289
Flavio Schoute <flavio03@outlook.com>
2021-09-21 19:59:10 +0200

    Update web.php
    Changed the welcome URL to the login, so the user automaticly redirects to the login page

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
